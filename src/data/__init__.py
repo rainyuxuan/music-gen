c20a9dda2a5e2ffd87d99017d78156e8aa5ba316
rainyuxuan <yu2001xuan@outlook.com>
2024-03-25 04:41:43 -0400

    Fixed the pipeline from wav to spectrogram

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -2,5 +2,23 @@
 Download or generate data.
 """
 
+from dataclasses import dataclass
+
 from .dataset import *
+
+
 # from loader import *
+
+
+@dataclass
+class Datasets:
+    train: Dataset
+    val: Dataset
+    test: Dataset
+
+
+@dataclass
+class DataLoaders:
+    train: DataLoader
+    val: DataLoader
+    test: DataLoader
--- src/models/trainer.py
+++ src/models/trainer.py
@@ -1,9 +1,10 @@
-from typing import Dict, List
+from custom_types import Dict, List
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 import pandas as pd
+
 
 class Trainer:
     """
@@ -18,32 +19,26 @@ class Trainer:
     dataloaders : Dict[str, DataLoader]
         A dictionary containing training, validation, and test dataloaders
     """
+
     model: nn.Module
     dataloaders: Dict[str, DataLoader]
-
 
     """Initialize the trainer
 
     The model is initialized as a default model, and the dataloaders are empty
     """
+
     def __init__(self):
         self.model = nn.Module()
-        self.dataloaders = {"train": None,
-                            "val": None,
-                            "test": None}
-
-
-
+        self.dataloaders = {"train": None, "val": None, "test": None}
 
     """Create the model
     
     Set the model to the model in use
     """
+
     def create_model(self) -> None:
         pass
-
-
-
 
     """Set dataloaders
     
@@ -58,13 +53,13 @@ class Trainer:
     test_loader : DataLoader
         Dataloader that contain the testing data
     """
-    def set_dataloaders(self, train_loader: DataLoader, val_loader: DataLoader, test_loader: DataLoader) -> None:
+
+    def set_dataloaders(
+        self, train_loader: DataLoader, val_loader: DataLoader, test_loader: DataLoader
+    ) -> None:
         self.dataloaders["train"] = train_loader
         self.dataloaders["val"] = val_loader
         self.dataloaders["test"] = test_loader
-
-
-
 
     """Calculate accuray for a given output and target
 
@@ -82,15 +77,12 @@ class Trainer:
     ----------
     Accuracy of the output
     """
+
     def get_accuracy(self, output: torch.Tensor, target: torch.Tensor) -> float:
         # Calculate accuracy by finding cos similiraty
         cos = nn.CosineSimilarity(dim=1, eps=1e-6)
 
-
         return torch.mean(torch.abs(cos(output, target)))
-    
-
-
 
     """Train the model
 
@@ -113,17 +105,20 @@ class Trainer:
     ---------
     A pd.DataFrame containing four columns: train_loss  val_loss  train_acc   val_acc
     """
-    def train(self,
-              weight_decay: float,
-              learning_rate: float,
-              num_epochs: int,
-              device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")) -> pd.DataFrame:
-        
-        model = self.model.to(device) # move model to GPU if applicable
+
+    def train(
+        self,
+        weight_decay: float,
+        learning_rate: float,
+        num_epochs: int,
+        device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
+    ) -> pd.DataFrame:
+
+        model = self.model.to(device)  # move model to GPU if applicable
         criterion = nn.MSELoss()
-        optimizer = optim.Adam(model.parameters(),
-                               lr=learning_rate,
-                               weight_decay=weight_decay)
+        optimizer = optim.Adam(
+            model.parameters(), lr=learning_rate, weight_decay=weight_decay
+        )
         history = []
 
         for e in range(num_epochs):
@@ -135,8 +130,8 @@ class Trainer:
 
             model.train()
 
-            for data, target in self.dataloaders['train']:
-                
+            for data, target in self.dataloaders["train"]:
+
                 if torch.cuda.is_available():
                     data, target = data.cuda(), target.cuda()
 
@@ -160,14 +155,13 @@ class Trainer:
                 # Multiply average accuracy times the number of examples in batch
                 train_acc += accuracy.item() * data.size(0)
 
-            
             # Don't need to keep track of gradients
             with torch.no_grad():
                 # Set to evaluation mode
                 model.eval()
 
                 # Validation loop
-                for data, target in self.dataloaders['val']:
+                for data, target in self.dataloaders["val"]:
                     # Tensors to gpu
                     data, target = data.cuda(), target.cuda()
 
@@ -186,25 +180,29 @@ class Trainer:
                     val_acc += accuracy.item() * data.size(0)
 
                 # Calculate average losses
-                train_loss = train_loss / len(self.dataloaders['train'].dataset)
-                val_loss = val_loss / len(self.dataloaders['val'].dataset)
+                train_loss = train_loss / len(self.dataloaders["train"].dataset)
+                val_loss = val_loss / len(self.dataloaders["val"].dataset)
 
                 # Calculate average accuracy
-                train_acc = train_acc / len(self.dataloaders['train'].dataset)
-                val_acc = val_acc / len(self.dataloaders['val'].dataset)
+                train_acc = train_acc / len(self.dataloaders["train"].dataset)
+                val_acc = val_acc / len(self.dataloaders["val"].dataset)
 
-                print(f'\nEpoch: {e} \tTraining Loss: {train_loss:.4f} \tValidation Loss: {val_loss:.4f}')
-                print(f'\t \tTraining Accuracy: {100 * train_acc:.2f}% \tValidation Accuracy: {100 * val_acc:.2f}%')
+                print(
+                    f"\nEpoch: {e} \tTraining Loss: {train_loss:.4f} \tValidation Loss: {val_loss:.4f}"
+                )
+                print(
+                    f"\t \tTraining Accuracy: {100 * train_acc:.2f}% \tValidation Accuracy: {100 * val_acc:.2f}%"
+                )
                 history.append([train_loss, val_loss, train_acc, val_acc])
-        
-        return pd.DataFrame(history, columns=['train_loss', 'val_loss', 'train_acc', 'val_acc'])
-    
 
-
+        return pd.DataFrame(
+            history, columns=["train_loss", "val_loss", "train_acc", "val_acc"]
+        )
 
     """Test the performance of the model
 
     TODO: implement this after we know how to test the model
     """
+
     def test(self):
         pass
